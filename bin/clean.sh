ecbe34cc85da4887bec5ceeb1164c6e652b8bbe4
jroehl <johann.roehl@contentful.com>
2019-09-03 20:34:33 +0200

    fix(relase): clean script

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env
 
-if type gsed &>/dev/null; then
+if command -v gsed >/dev/null 2>&1; then
   for filename in "$(pwd)/docs/*.md"; do
     gsed -i 's/v0.0.0/master/g' $filename
   done
